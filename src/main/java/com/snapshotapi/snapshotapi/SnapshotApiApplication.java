c13a8a5b5a66694013dcfa0bc14b65aeae881fe7
Marcos <magma_ruby145@hotmail.com>
2023-09-18 06:48:35 -0700

    Initial commit

---
+++ src/main/java/com/snapshotapi/snapshotapi/SnapshotApiApplication.java
@@ -0,0 +1,13 @@
+package com.snapshotapi.snapshotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapshotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapshotApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snapshotapi/snapshotapi/SnapshotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snapshotapi.snapshotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapshotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
